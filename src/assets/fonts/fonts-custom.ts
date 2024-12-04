57f33f19b30aec5c86eb21266cdb07273620209e
Scriptscrypt <tirumaladasa@gmail.com>
2024-12-04 12:48:20 +0530

    Feat: Init Font and Assets setup

---
+++ src/assets/fonts/fonts-custom.ts
@@ -0,0 +1,7 @@
+import localFont from "next/font/local";
+
+export const relishGargler = localFont({
+  src: "./RelishGargler/relish-gargler-regular.otf",
+  variable: "--font-relishGargler",
+  weight: "100 900",
+});
