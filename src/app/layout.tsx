7581a5637a8def44573d052efd1a30c49ada2286
aryan <agarwalaryan139@gmail.com>
2024-12-10 03:53:14 +0530

    feat: og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,29 @@ const ppSans = localFont({
 
 export const metadata: Metadata = {
   title: 'Solana AI Hackathon',
-  description: 'Accelerating the Solana AI Ecosystem',
+  description: '10th - 23rd Dec. global hackathon. $100k+ in prizes',
+  openGraph: {
+    title: 'Solana AI Hackathon',
+    description: '10th - 23rd Dec. global hackathon. $100k+ in prizes',
+    url: 'https://solanaaihackathon.com',
+    siteName: 'Solana AI Hackathon',
+    images: [
+      {
+        url: '/og.jpeg',
+        width: 1200,
+        height: 630,
+        alt: 'Solana AI Hackathon',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Solana AI Hackathon',
+    description: '10th - 23rd Dec. global hackathon. $100k+ in prizes',
+    images: ['/og.jpeg'],
+  },
 };
 
 export default function RootLayout({
