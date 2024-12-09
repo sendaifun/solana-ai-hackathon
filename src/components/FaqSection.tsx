462236c56bc89e8f1ac8c4f681c19b93986d50ad
Scriptscrypt <tirumaladasa@gmail.com>
2024-12-09 23:47:33 +0530

    Fix: FAQ Questions

--- src/components/FaqSection.tsx
+++ src/components/FaqSection.tsx
@@ -79,7 +79,7 @@ export default function FAQSection() {
                         data-[state=open]:border-cyan-400/40 data-[state=open]:bg-[#191F26] data-[state=open]:text-white transition-all group"
               >
                 <span className="text-xl font-medium tracking-wider text-gray-400  font-relishGargler">
-                  FREQUENTLY ASKED QUESTION - {index + 1}
+                  {faq.question}
                 </span>
                 <div className="shrink-0 ml-2">
                   {/* Custom Toggle Icons */}
@@ -108,19 +108,68 @@ export default function FAQSection() {
 
 const faqData = [
   {
+    question: "What is the Solana AI Hackathon?",
     content:
-      "WE'RE WORKING ON PROVIDING YOU WITH THE MOST ACCURATE AND DETAILED INFORMATION. PLEASE CHECK BACK SOON AS THIS SECTION WILL BE UPDATED SHORTLY WITH ALL THE ANSWERS YOU NEED!",
+      "The Solana AI Hackathon is a global online event focused on building AI agents and tooling on Solana, powered by SEND AI and supported by leading teams in the Solana ecosystem. Prizes range from $5k to $30k, aiming to encourage serious Crypto x AI projects capable of raising VC funding or launching their own tokens.",
   },
   {
+    question: "How many tracks can I submit?",
     content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "You can submit your project in any two tracks of your choice. For example, if you've built a Telegram group chat-based trading AI agent, you can submit it to both the DeFi Agents Track and the Trading Agents Track. All projects are automatically eligible for the Main Track, which represents the best overall project.",
   },
   {
+    question: "Do we have to integrate a particular sponsor?",
     content:
-      "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "No, you are free to build whatever you want, as long as it aligns with the theme. However, we strongly recommend integrating our sponsor SDKs/products, as this provides priority support and opens up future collaboration opportunities.",
   },
   {
+    question: "What should be the team size?",
     content:
-      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      "1-5. You can submit entries either individually or as part of a team of 2-5 members. Ideally, we recommend a mix of AI developers, crypto developers, and marketing/business professionals. However, the product you deliver >> the team.",
+  },
+  {
+    question: "When will the submission open?",
+    content:
+      "Submissions will open on 15th December via a form where you can provide all your project details, including your GitHub repository, a 2-minute demo/pitch, a 10-slide pitch deck, and more.",
+  },
+  {
+    question: "What's the judging criteria?",
+    content:
+      "Just like any pitch competition, you'll be judged on: 1) Product demo and pitch 2) Business viability 3) AI models used 4) Solana integrations 5) Potential impact 6) Traction and marketing 7) Innovation (the crazier, the better). Bonus points will be awarded for integrating sponsor solutions and open-sourcing products. Category-defining products will be given higher preference.",
+  },
+  {
+    question: "Who will own the IP of the project?",
+    content:
+      "It's 100% yours. Unless you raise funds or grant advisory equity during the course of the hackathon, you retain complete responsibility for your idea and project IP.",
+  },
+  {
+    question: "Where can I access all mentorship and meet other teams?",
+    content: "Telegram! Join the Solana AI Hackathon telegram to stay updated.",
+  },
+  {
+    question: "Is it completely online?",
+    content:
+      "While the interaction submissions are online, there will be 6-10 IRL pop-ups around the world via local Superteam chapters and Superhosts –– to help you connect with builders.",
+  },
+  {
+    question: "Who's organising this?",
+    content:
+      "This is a Solana ecosystem-led hackathon, with prizes contributed by top ecosystem teams and curated by SEND AI. P.S: The Solana Foundation is just one of the sponsors.",
+  },
+  {
+    question:
+      "Will all projects who submit a hackathon project receive feedback?",
+    content:
+      "We can't guarantee, but depending on the volume of hackathon submissions we receive, we will try our best to ensure all project submissions will be reviewed by the SEND AI team and/or the judging panel.",
+  },
+  {
+    question: "May I submit multiple projects?",
+    content:
+      "Yes, you can submit multiple projects. However, we recommend focusing on a single project, as the hackathon will be highly competitive. This approach allows you to concentrate on developing your best product concept and maximizing your chances of winning at least one prize!",
+  },
+  {
+    question: "Where can I ask technical questions during the hackathon?",
+    content:
+      "In the telegram group. Further, feel free to use twitter as a cheat code –– you can directly post your questions as a tweet and just tag @sendaifun, we'll tag relevant experts.",
   },
 ];
