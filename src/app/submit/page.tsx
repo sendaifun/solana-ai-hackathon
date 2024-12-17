e7824fc919eb77296800361b31b1a3a226a4cb57
Scriptscrypt <tirumaladasa@gmail.com>
2024-12-17 13:14:30 +0530

    Feat: Typeform Submissions

---
+++ src/app/submit/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Widget } from "@typeform/embed-react";
+
+const page = () => {
+  return (
+    <>
+      <div className="w-[100vw] h-[100vh]">
+        <Widget
+          id="MXJRmO6x"
+          style={{ width: "100%", height: "100%" }}
+          className="my-form"
+        />
+      </div>
+    </>
+  );
+};
+
+export default page;
