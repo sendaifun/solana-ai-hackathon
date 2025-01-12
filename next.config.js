4c75a53507190f50fd7dd5ee7096904a7465b0b5
Scriptscrypt <tirumaladasa@gmail.com>
2025-01-12 11:36:11 +0530

    Fix: Disclaimer & Revert

--- next.config.js
+++ next.config.js
@@ -1,40 +1,8 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-      {
-        protocol: "http",
-        hostname: "**",
-      },
-    ],
-  },
-  async headers() {
-    return [
-      {
-        // This adds CORS headers for cross-domain requests
-        source: "/dashboard/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://solanaaiindex.com", // Adjust if using different protocol
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-Requested-With, Content-Type, Authorization",
-          },
-        ],
-      },
-    ];
+    domains: ["ik.imagekit.io"],
   },
 };
 
